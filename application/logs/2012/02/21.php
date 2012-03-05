90863d1fd4caeb168048011b030b4d9f5d9d143e
Michał Malisz <malisz.m@gmail.com>
2012-03-05 11:52:46 +0100

    framework

---
+++ application/logs/2012/02/21.php
@@ -0,0 +1,42 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-02-21 00:05:04 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-02-21 00:05:04 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /var/www/kohana/kohana-3.2-master-1/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-02-21 00:05:05 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-02-21 00:05:05 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /var/www/kohana/kohana-3.2-master-1/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-02-21 00:05:05 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-02-21 00:05:05 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /var/www/kohana/kohana-3.2-master-1/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-02-21 00:05:05 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-02-21 00:05:05 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /var/www/kohana/kohana-3.2-master-1/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-02-21 00:05:05 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-02-21 00:05:05 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /var/www/kohana/kohana-3.2-master-1/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-02-21 00:05:06 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-02-21 00:05:06 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /var/www/kohana/kohana-3.2-master-1/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-02-21 00:05:06 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-02-21 00:05:06 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /var/www/kohana/kohana-3.2-master-1/index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-02-21 00:05:06 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2012-02-21 00:05:06 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/kohana-3.2-master-1 ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /var/www/kohana/kohana-3.2-master-1/index.php(109): Kohana_Request->execute()
+#1 {main}
