1d18b2954268ccdac56d1359244985cdf7e01016
Michał Malisz <malisz.m@gmail.com>
2012-03-10 21:04:13 +0100

    menu

---
+++ menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <meta http-equiv="content-type" content="text/html; charset=iso-8859-2" />
+        <title>Ticket System</title>
+        <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+    </head>
+    <body>
+        
+        <div id="sidebar">
+            <ul>
+                <li>
+                    <div style="clear: both;">&nbsp;</div>
+                </li>
+                <li>
+                    <h2>Menu</h2>
+                    <ul>
+                        <li><a href="#">Przeglądaj zgłoszenia</a></li>
+                        <li><a href="#">Dodaj zgłoszenie</a></li>
+                        <li><a href="#">Zarządzaj zgłoszeniami</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </body>
+</html>
