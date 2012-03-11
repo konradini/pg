6918b732d6292b95e1d1473d51436f5df292acac
Michał Malisz <malisz.m@gmail.com>
2012-03-11 19:38:17 +0100

    footer

--- dodaj.php
+++ dodaj.php
@@ -132,6 +132,9 @@ Released   : 20100110
                 </div>
             </div>
         </div>
+        <div id="footer">
+            <p>Copyright (c) 2008 Sitename.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a></p>
+        </div>
     </body>
 </html>
 
--- register.php
+++ register.php
@@ -110,5 +110,8 @@
                     </div>
                 </div>
         </div>
+        <div id="footer">
+            <p>Copyright (c) 2008 Sitename.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a></p>
+        </div>
     </body>
 </html>
