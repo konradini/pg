1929bf90afb0189e173536517f69549eda0f8c6c
Michał Malisz <malisz.m@gmail.com>
2012-03-11 22:16:33 +0100

    szablon przegladaj/zarzadzaj

--- zarzadzaj.php
+++ zarzadzaj.php
@@ -12,9 +12,11 @@ Released   : 20100110
 -->
 <?php
     session_start();
-    if(isset($_SESSION['login'])){
+     if(isset($_SESSION['login'])){
         $login=$_SESSION['login'];
+        $klasa=$_SESSION['klasa'];
      }
+     $ref=$_SERVER['PHP_SELF'];
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
@@ -24,19 +26,84 @@ Released   : 20100110
         <title>Ticket System</title>
         <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
     </head>
-    <body>        
+    <body>
+        <div id="framecontentTop">
+            <div id="wrapper">
+            <div id="header-wrapper">
+                <div id="header">
+                    <div id="logo">
+                        <div style="color: white;">
+                        <form action="exec/login-exec.php" method="get">
+                            <table><tr>
+                                    <?php
+                                        if(!isset($login)){
+                                            echo "<td>Login:<input type='text' name='login'></input></td>
+                                                <td>Has³o:<input type='password' name='pass'></input></td>
+                                                <td><input type='hidden' value='$ref' name='ref'></input></td>
+                                                <td><input type='submit' value='OK'></input></td>";
+                                        }else{
+                                            echo "<td>Zalogowano jako <b>" .$login. "</b></td>";
+                                            echo "<td><a href='logout.php?ref=$ref'>[ wyloguj ]</a></td>";
+                                        }
+                                        if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
+                                            echo '<td>'; 
+                                            foreach ($_SESSION['ERRMSG_ARR'] as $msg){
+                                            echo '<li>' . $msg . '</li>';
+                                        }
+                                        echo '</td>';
+                                        unset($_SESSION['ERRMSG_ARR']);
+                                        }
+                                    ?>
+                            </tr></table>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div id="framecontentLeft">
+            <div id="sidebar">
+            <ul>
+                <li>
+                    <div style="clear: both;">&nbsp;</div>
+                </li>
+                <li>
+                    <h2>Menu</h2>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="przeglad.php">Przegl±daj zg³oszenia</a></li>
+                        <li><a href="dodaj.php">Dodaj zg³oszenie</a></li>
+                        <li><a href="zarzadzaj.php">Zarz±dzaj zg³oszeniami</a></li>
+                        <?php
+                            if(isset($login) && $klasa=='admin'){
+                                echo "<li><a href='register.php'>Dodaj u¿ytkownika</a></li>";
+                            }
+                        ?>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        </div>
             <div id="page">
                 <div id="page-bgtop">
                     <div id="page-bgbtm">
                         <div id="content">
+                            <div style="color: black">
                             <?php
                                 if(!isset($login)){
                                     echo "Brak dostêpu! Zaloguj siê";
                                 }
                             ?>
+                            </div>
                         </div>
+                        <!-- end #content -->
                     </div>
                 </div>
             </div>
+            <!-- end #page -->
+        <div id="footer">
+            <p>Copyright (c) 2008 Sitename.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a></p>
+        </div>
     </body>
 </html>
